079424bb99d3976adb9276ea2c8cd6fc7fe2a48e
ans1403 <akrnose1403@gmail.com>
2022-05-13 01:18:53 +0900

    refactor: main.goからシェルと設定読み込みを分割

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,23 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"golang-devcontainer/src/service"
-	"log"
-
-	"gopkg.in/ini.v1"
+	"golang-devcontainer/src/shell"
 )
 
 func main() {
-	cfg, err := ini.Load("../../config/config.ini")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	maxPrintCount := cfg.Section("Common").Key("max_print_count").MustInt()
-
-	calculateService := service.NewCalculate()
-	result := calculateService.Service(maxPrintCount)
-
-	fmt.Println(result)
+	shell := shell.NewCalculateShell()
+	shell.Execute()
 }
